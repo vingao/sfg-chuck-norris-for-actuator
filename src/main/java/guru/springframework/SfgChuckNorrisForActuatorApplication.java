ab1b26ba56ae420808022b102160a810fbe3c62f
Vincent Gao <a202898@fmr.com>
2018-10-09 21:09:54 -0400

    initial setup; closes #1

---
+++ src/main/java/guru/springframework/SfgChuckNorrisForActuatorApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgChuckNorrisForActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgChuckNorrisForActuatorApplication.class, args);
+    }
+}
---
+++ src/main/java/guru/springframework/norris/chuck/ChuckNorrisQuotes.java
@@ -0,0 +1,7 @@
+package guru.springframework.norris.chuck;
+
+public class ChuckNorrisQuotes {
+    public String getRandomQuote() {
+        return "joke 1";
+    }
+}
