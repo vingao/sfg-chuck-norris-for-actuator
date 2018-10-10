0b5fba8a454a9e98fe4437a6cc651ef5b32abf62
Vincent Gao <a202898@fmr.com>
2018-10-09 21:31:23 -0400

    generating random jokes; closes #3

--- src/main/java/guru/springframework/norris/chuck/ChuckNorrisQuotes.java
+++ src/main/java/guru/springframework/norris/chuck/ChuckNorrisQuotes.java
@@ -1,7 +1,19 @@
 package guru.springframework.norris.chuck;
 
+import java.util.Random;
+
 public class ChuckNorrisQuotes {
     public static String getRandomQuote() {
-        return "joke 1";
+        return "joke " + getRandomNumberInRange(1, 100);
+    }
+
+    private static int getRandomNumberInRange(int min, int max) {
+
+        if (min >= max) {
+            throw new IllegalArgumentException("max must be greater than min");
+        }
+
+        Random r = new Random();
+        return r.nextInt((max - min) + 1) + min;
     }
 }
